459dc532163dd7a648f945b5dc4c9d5e2523325c
erebuseternal <marcelsanders96@gmail.com>
2021-02-02 15:44:24 +0000

    basic login view

--- webapp/main/views.py
+++ webapp/main/views.py
@@ -4,3 +4,16 @@ from . import main
 @main.route('/', methods=['GET'])
 def home():
     return render_template('home.html')
+
+@main.route('/login', methods=['GET'])
+def login():
+    site = "https://www.inaturalist.org"
+    app_id = 'biotascape'
+    app_secret = 'biotasecret'
+    redirect_uri = 'https://localhost:5000/welcome'
+    url = f'{site}/oauth/authorize?client_id={app_id}&redirect_uri={redirect_uri}&response_type=code'
+    return render_template('login.html', url=url)
+
+@main.route('/naturalist_login', methods=['GET'])
+def naturalist_login():
+    return render_template('home.html')
