ccf7b0700f3193f764a49eab2865bb4683132ed3
erebuseternal <marcelsanders96@gmail.com>
2021-02-09 02:10:58 +0000

    login tutorial complete

--- webapp/main/views.py
+++ webapp/main/views.py
@@ -1,6 +1,12 @@
 from flask import render_template
+from flask_login import login_required, current_user
 from . import main
 
 @main.route('/', methods=['GET'])
 def home():
     return render_template('home.html')
+
+@main.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html', name=current_user.name)
